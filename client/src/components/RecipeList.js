dd7516519a080c2b412358b079d89928968bd433
cw417 <chriswhitlock417@gmail.com>
2022-09-05 19:05:14 -0400

    Start working on Navbar and RecipeList

--- client/src/components/RecipeList.js
+++ client/src/components/RecipeList.js
@@ -1,7 +1,23 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 export default function RecipeList() {
+
+  const [recipes, setRecipes] = useState([])
+
+  function recipeList() {
+    return recipes.map((recipe, index) => {
+      <div key={index}>
+        {recipe.name}
+      </div>
+    })
+  }
+
   return (
-    <div>RecipeList</div>
+    <div className='flex flex-col items-center'>
+      <div className='text-3xl mt-2'>
+        Recipes
+      </div>
+      <div>{recipeList()}</div>
+    </div>
   )
 }
