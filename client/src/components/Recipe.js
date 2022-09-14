3f606e97adcfce365683ab260486558fe530330b
cw417 <chriswhitlock417@gmail.com>
2022-09-14 15:34:34 -0400

    Add select to display to Recipe

--- client/src/components/Recipe.js
+++ client/src/components/Recipe.js
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import IngredientList from './IngredientList';
 import InstructionList from './InstructionList';
 
 export default function Recipe({ recipe }) {
 
+  const [ display, setDisplay ] = useState('none')
+  
+  function handleSelect() {
+    /**
+     * Toggles display of the RecipeInfo component on/off.
+     */
+    if (display === 'none') {
+      setDisplay('block')
+      console.log('selected ' + recipe.name)
+    } else {
+      setDisplay('none')
+      console.log('unselected ' + recipe.name)
+    }
+  }
+
+
   return (
-    <div>
+    <div onClick={handleSelect}>
       {recipe.name}
-      <div className='ml-4'>
+      <div className='ml-4' style={{display:display}}>
         <div>
           <div>Ingredients</div>
           <IngredientList
