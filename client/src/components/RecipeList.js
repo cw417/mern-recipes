d758eb973b944c355ba70e997f9380899fb0b5c9
cw417 <chriswhitlock417@gmail.com>
2022-09-14 16:08:37 -0400

    Add search and restore for RecipesList

--- client/src/components/RecipeList.js
+++ client/src/components/RecipeList.js
@@ -8,25 +8,63 @@ export default function RecipeList() {
   const [filter, setFilter] = useState(false);
 
   useEffect(() => {
-    async function getRecipes() {
-      const response = await fetch(`http://localhost:5000/recipes/`);
-  
-      if (!response.ok) {
-        const message = `An error occurred: ${response.statusText}`;
-        window.alert(message);
-        return;
+    if (!filter) {
+      async function getRecipes() {
+        const response = await fetch(`http://localhost:5000/recipes/`);
+    
+        if (!response.ok) {
+          const message = `An error occurred: ${response.statusText}`;
+          window.alert(message);
+          return;
+        }
+    
+        const recipes = await response.json();
+        setRecipes(recipes);
       }
-  
-      const recipes = await response.json();
-      setRecipes(recipes);
+    
+      getRecipes();
+    
+      console.log(recipes)
+      return;
     }
-  
-    getRecipes();
-  
-    console.log(recipes)
-    return;
-  }, [recipes.length]);
-  
+  }, [recipes.length, filter]);
+
+  function toggleFilter() {
+    /**
+     * Toggles the filter state.
+     */
+    const newFilter = !filter;
+    setFilter(newFilter);
+  }
+
+  function searchRecipes(keyword) {
+    /**
+     * Searches each recipe name and ingredients list
+     * for strings containing the given keyword.
+     */
+    if (filter) { return } else { toggleFilter() };
+    const newRecipes = recipes.filter(recipe => recipe.name.includes(keyword) || searchIngredients(keyword, recipe));
+    setRecipes(newRecipes);
+  }
+    
+  function searchIngredients(query, recipe) {
+    /**
+     * Searches given 'recipe' object's 'ingredients' array for given query.
+     * Matches are case insensitive.
+     * @param  {String}  query   String to search for.
+     * @param  {Object}  recipe  Recipe to search.
+     * @return {Boolean}         True if ingredient name contains the given query string. 
+     */
+    let found = false
+    recipe.ingredients.forEach(ingredient => {
+      console.log(`searching for ${query} in ${recipe.name}`)
+      if (ingredient.name.toLowerCase().includes(query)) {
+        found = true
+      }
+    })
+    return found
+  }
+
   function recipeList() {
     /**
      * Map recipes to a list of recipe components.
@@ -47,7 +85,7 @@ export default function RecipeList() {
       <div className='text-3xl mt-2'>
         Recipes
       </div>
-      <SearchBar />
+      <SearchBar searchRecipes={searchRecipes} toggleFilter={toggleFilter} />
       <div>{recipeList()}</div>
     </div>
   )
--- client/src/components/SearchBar.js
+++ client/src/components/SearchBar.js
@@ -1,19 +1,28 @@
 import React, { useRef } from 'react';
 import { FiSearch, FiRotateCcw } from 'react-icons/fi';
 
-export default function SearchBar({ searchRecipes, restoreRecipes }) {
+export default function SearchBar({ searchRecipes, toggleFilter }) {
 
   const inputRef = useRef();
 
   function handleSearch(){
+    /**
+     * Calls the searchRecipes function and passes in the keyword to search for.
+     */
     const keyword = inputRef.current.value;
+    if (keyword === '') return;
     searchRecipes(keyword);
     console.log(`Searching for: ${keyword}`);
     inputRef.current.value = null;
   }
 
   function handleRestore(){
-    restoreRecipes();
+    /**
+     * Calls toggleFilter function, which toggles the 'filter' state in RecipesList.
+     * The 'filter' state is watched by a useEffect hook that will restore the recipes
+     * array from the database.
+     */
+    toggleFilter();
     console.log(`Restoring.`);
     inputRef.current.value = null;
   }
